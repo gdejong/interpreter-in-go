8c8a2ea2e68143d9b00ffc3ac419211c8bb4023f
gdejong <gijs_d.j@hotmail.com>
2023-09-21 14:59:03 +0200

    Added more one-character tokens

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -29,8 +29,15 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
